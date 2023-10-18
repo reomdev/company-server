e355433f5605f9dc20dbce783f01df15ac1cc5bb
Roger Obando <reobando12@gmail.com>
2023-10-17 21:27:54 -0400

    [fix] update url mongodb connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,8 +9,7 @@ import { join } from 'path';
 
 @Module({
   imports: [
-    // MongooseModule.forRoot('mongodb://mongo-company-server:27017/company'),
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/company'),
+    MongooseModule.forRoot('mongodb://mongo-company-server:27017/company'),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
